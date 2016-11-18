c201f93298f60c5f547e89319efdd574afde9d5b
Brad Chao <brad@brad.tw>
2016-11-18 09:41:26 +0800

    v3

--- app/src/main/java/com/example/user/brad08/BradView.java
+++ app/src/main/java/com/example/user/brad08/BradView.java
@@ -2,11 +2,13 @@ package com.example.user.brad08;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.view.View;
 
 public class BradView extends View {
     public BradView(Context context) {
         super(context);
+        setBackgroundColor(Color.YELLOW);
     }
 
     @Override
--- app/src/main/java/com/example/user/brad08/GameView.java
+++ app/src/main/java/com/example/user/brad08/GameView.java
@@ -2,12 +2,14 @@ package com.example.user.brad08;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class GameView extends View {
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setBackgroundColor(Color.GREEN);
     }
 
     @Override
