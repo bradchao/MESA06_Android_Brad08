fb2a46079ebd031cab386d73c86c709418b3daac
Brad Chao <brad@brad.tw>
2016-11-18 09:19:35 +0800

    v1

---
+++ app/src/main/java/com/example/user/brad08/GameView.java
@@ -0,0 +1,17 @@
+package com.example.user.brad08;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class GameView extends View {
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
