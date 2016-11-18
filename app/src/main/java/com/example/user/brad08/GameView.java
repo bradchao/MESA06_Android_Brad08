3c38c7ed2933699c6606d51039c0643f97d3c270
Brad Chao <brad@brad.tw>
2016-11-18 10:06:55 +0800

    v5

--- app/src/main/java/com/example/user/brad08/GameView.java
+++ app/src/main/java/com/example/user/brad08/GameView.java
@@ -1,19 +1,27 @@
 package com.example.user.brad08;
 
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class GameView extends View {
+    private Resources res;
+
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setBackgroundColor(Color.GREEN);
+        res = context.getResources();
+        //setBackgroundColor(Color.GREEN);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        Bitmap ball = BitmapFactory.decodeResource(res,R.drawable.brad);
+        canvas.drawBitmap(ball,0,0,null);
     }
 }
