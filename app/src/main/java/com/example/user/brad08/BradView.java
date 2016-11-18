2ab2b0470ea01b9bf0841a751a4fc369c5f9a4ee
Brad Chao <brad@brad.tw>
2016-11-18 09:31:20 +0800

    v2

---
+++ app/src/main/java/com/example/user/brad08/BradView.java
@@ -0,0 +1,16 @@
+package com.example.user.brad08;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class BradView extends View {
+    public BradView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
