20c8b1362a4255706baff9072ebe0a95c06cac7b
Brad Chao <brad@brad.tw>
2016-11-18 11:33:26 +0800

    v9

--- app/src/main/java/com/example/user/brad08/GameView.java
+++ app/src/main/java/com/example/user/brad08/GameView.java
@@ -11,6 +11,8 @@ import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 
+import java.util.Timer;
+
 public class GameView extends View {
     private Resources res;
     private Context context;
@@ -19,13 +21,14 @@ public class GameView extends View {
     private Bitmap bmpBall;
     private Matrix matrix;
     private float ballW, ballH;
+    Timer timer;
 
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
         this.context = context;
         res = context.getResources();
         matrix = new Matrix();
-
+        timer = new Timer();
     }
 
     private void init(){
