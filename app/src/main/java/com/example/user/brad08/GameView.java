fd96369ab3b661d699a2a22eda14ed71d8841c2e
Brad Chao <brad@brad.tw>
2016-11-18 10:37:23 +0800

    v6

--- app/src/main/java/com/example/user/brad08/GameView.java
+++ app/src/main/java/com/example/user/brad08/GameView.java
@@ -7,21 +7,38 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 public class GameView extends View {
     private Resources res;
+    private Context context;
+    private int viewW, viewH;
+    private boolean isInit;
+    private Bitmap bmpBall;
 
     public GameView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.context = context;
         res = context.getResources();
         //setBackgroundColor(Color.GREEN);
+
+
+    }
+
+    private void init(){
+        viewW = getWidth(); viewH = getHeight();
+        Log.v("brad", viewW + "x" +viewH);
+
+        bmpBall = BitmapFactory.decodeResource(res,R.drawable.brad);
+        isInit = true;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Bitmap ball = BitmapFactory.decodeResource(res,R.drawable.brad);
-        canvas.drawBitmap(ball,0,0,null);
+        if (!isInit) init();
+
+        canvas.drawBitmap(bmpBall,0,0,null);
     }
 }
